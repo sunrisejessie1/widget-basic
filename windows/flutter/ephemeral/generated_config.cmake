f50712bb5c1190d2c81c553492724fb27ba87b69
sunrisejessie1 <jessica.cedillo.alp@cbtis258.edu.mx>
2025-04-02 09:15:33 -0600

    Archivos del Proyectos Cargados
    Tuve que cargar los archivos de forma separada de no tener problema.

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\FLUTTER2025\\03_Flutter\\flutter_app_2" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "PROJECT_DIR=D:\\FLUTTER2025\\03_Flutter\\flutter_app_2"
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\FLUTTER2025\\03_Flutter\\flutter_app_2\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\FLUTTER2025\\03_Flutter\\flutter_app_2"
+  "FLUTTER_TARGET=D:\\FLUTTER2025\\03_Flutter\\flutter_app_2\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\FLUTTER2025\\03_Flutter\\flutter_app_2\\.dart_tool\\package_config.json"
+)
